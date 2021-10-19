5f0dda775df36920bd896882fc03a769618886b3
Khatib Muhammad Talha (MS/EXM31-XC) <uhh2kor@bosch.com>
2021-10-19 09:58:21 +0530

    Updated statistics and statistics.test - 1

--- statistics.py
+++ statistics.py
@@ -1,3 +1,15 @@
-
 def calculateStats(numbers):
-  return None
+    try:
+        avg = sum(numbers) / len(numbers)
+        numbers.sort()
+        max = numbers[-1]
+        min = numbers[0]
+    except ZeroDivisionError:
+        avg = 0
+        max = 0
+        min = 0
+    computedStats = {'avg': avg, 'max': max, 'min': min}
+    return computedStats
+
+
+
